bf45c2d23244c44c45e2995c77a88e608c75be4e
SeokHo Kim <kim76744@naver.com>
2021-05-28 01:06:54 +0900

    Java를 자바 4일차

---
+++ Java를 자바 6월 과정/src/hello/HelloWorld.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "Java를 자바 4일차" ;
+		int num1 = 4 ;
+		long num2 = 2200000000l;
+		float num3 = 3.1415926535897932384626433832795028841971f ;
+		double num4 = 3.1415926535897932384626433832795028841971 ;
+		boolean b = false;
+		boolean c = true;
+		
+		System.out.println(a);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
