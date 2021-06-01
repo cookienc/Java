92c5669827998136ab53e95411b712fa56a30f2a
SeokHo Kim <kim76744@naver.com>
2021-06-01 11:49:00 +0900

    JavaÀڹÙ 6¿ù °úÁ¤ 7ÀÏÂ÷

--- Java를 자바 6월 과정/src/hello/HelloWorld.java
+++ Java를 자바 6월 과정/src/hello/HelloWorld.java
@@ -7,21 +7,17 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in); // 객체 생성
 		
-		System.out.println("========자기소개========");
-			
-		System.out.print("이름을 입력해주세요 : ");
-		String sentence1 = sc.nextLine();
-				
-		System.out.print("모각코 어떤 과정을 하고 있나요? : ");
-		String sentence2 = sc.nextLine();
-		
-		System.out.print("모각코 며칠째 과정인가요? : ");
+		System.out.print("첫번째 숫자를 입력하세요: ");
 		int num1 = sc.nextInt();
+		System.out.print("두번째 숫자를 입력하세요: ");
+		int num2 = sc.nextInt();
 		
-		System.out.print("모각코 과정 몇 개나 참여했나요? : ");	
-		float num2 = sc.nextFloat();
-		
-		System.out.printf("%s님 모각코 %s 과정 %d일차, %.1f번째 과정입니다.\n", sentence1, sentence2, num1, num2);	
+		System.out.println("===사칙연산 출력하기===");
+		System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+		System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+		System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+		System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+		System.out.printf("%d %% %d = %d\n", num1, num2, num1 % num2);
 		
 		sc.close(); //잊지 말기!!
 	}
