61bc7ccf467021a438f59933a8771515bc8b704f
SeokHo Kim <kim76744@naver.com>
2021-06-02 10:53:22 +0900

    Java¸¦ ÀڹÙ 6¿ù °úÁ¤ 8ÀÏÂ÷

--- Java를 자바 6월 과정/src/hello/HelloWorld.java
+++ Java를 자바 6월 과정/src/hello/HelloWorld.java
@@ -5,19 +5,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in); // 객체 생성
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("첫번째 숫자를 입력하세요: ");
-		int num1 = sc.nextInt();
-		System.out.print("두번째 숫자를 입력하세요: ");
-		int num2 = sc.nextInt();
-		
-		System.out.println("===사칙연산 출력하기===");
-		System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
-		System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
-		System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
-		System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
-		System.out.printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+		System.out.print("숫자 2개를 입력하세요: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.print("둘 중에 큰 수는 : ");
+		System.out.println( a > b ? a : (a == b) ? "같음" : b);
 		
 		sc.close(); //잊지 말기!!
 	}
