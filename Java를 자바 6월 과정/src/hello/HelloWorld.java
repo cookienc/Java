08063846e4c4b9c0c91988705c3a7a444a85f585
SeokHo Kim <kim76744@naver.com>
2021-06-08 11:57:17 +0900

    Java를 자바 6월 과정 12일차 - 클래스 개념

--- Java를 자바 6월 과정/src/hello/HelloWorld.java
+++ Java를 자바 6월 과정/src/hello/HelloWorld.java
@@ -1,32 +1,41 @@
 package hello;
 
-import java.util.Scanner; // 스캐너 복사
+class Calculator {
+	int x, y;
+	
+	public int sum() {
+		return x + y ;	
+	}
+	public int sub() {
+		return x - y;
+	}
+	public int div() {
+		return x / y;
+	}
+	public int mul() {
+		return x * y;
+	}
+	public void printResult() {
+		System.out.printf("%d + %d = %d\n", x, y, sum());
+		System.out.printf("%d - %d = %d\n", x, y, sub());
+		System.out.printf("%d / %d = %d\n", x, y, div());
+		System.out.printf("%d * %d = %d\n", x, y, mul());
+	}
+}
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("숫자 5개를 띄어쓰기로 구분하여 입력하세요 : ");
-		int[] arr = new int[5];
-		
-		for(int i = 0; i<5 ; i++) {
-		
-		arr[i] = sc.nextInt();
-			
-		}
-		System.out.println("=====입력완료=====");
-		System.out.println("=====출력시작=====");
-		
-		for(int i = 4; i >= 0 ;i--) {
-			
-			System.out.println(arr[i]);
-		}
-		
-		
-		
 	
-		sc.close(); //잊지 말기!!
+		Calculator calculator = new Calculator();
+		
+		calculator.x = 30;
+		calculator.y = 50;
+
+		calculator.printResult();
+		
+
 	}
 
 }
