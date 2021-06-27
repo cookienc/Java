56b1f7aa982f9da70277fd1a6cb0a23635b678bc
SeokHo Kim <kim76744@naver.com>
2021-06-27 12:09:51 +0900

    refactor : Refactoring Gugudan
    클래스를 이용하여 구구단을 구현

--- 만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan/src/Gugudan.java
+++ 만들어 가면서 배우는 JAVA 플레이그라운드/Gugudan/src/Gugudan.java
@@ -1,5 +1,4 @@
 public class Gugudan {
-	
 	public static int[] calculate(int times) {
 		int[] result = new int[9];
 		for(int i = 0; i < result.length; i++) {
@@ -11,13 +10,6 @@ public class Gugudan {
 	public static void print(int[] result) {
 		for(int tmp : result) {
 			System.out.println(tmp);
-		}
-	}
-	
-	public static void main(String[] args) {
-		for(int i = 2; i < 10; i++) {
-			int[] result = calculate(i);
-			print(result);
 		}
 	}
 }
