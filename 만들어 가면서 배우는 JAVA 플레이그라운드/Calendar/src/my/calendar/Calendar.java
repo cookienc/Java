94cffb0a084b385b6810fed498f1b817186e14a7
SeokHo Kim <kim76744@naver.com>
2021-06-28 08:13:07 +0900

    refactor : Refacotring repeat structure

--- 만들어 가면서 배우는 JAVA 플레이그라운드/Calendar/src/my/calendar/Calendar.java
+++ 만들어 가면서 배우는 JAVA 플레이그라운드/Calendar/src/my/calendar/Calendar.java
@@ -23,14 +23,16 @@ public class Calendar {
 	public static void main(String[] args) {
 	
 		//숫자를 입력받아 해당월의 최대 몇일로 구성되어 있는지 출력하는 프로그램 작성
+		
+		String PROMPT = "cal>";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
 		
-		for(int i = 0; i < repeat ; i++) {
-			System.out.println("달을 입력하세요");
+		while(true) {
+			System.out.println("월을 입력하세요");
+			System.out.print(PROMPT);
 			int month = scanner.nextInt();
+			if (month < 1 || month > 12) break;
 			System.out.printf("%d월은 %d일까지 있습니다. %n", month, cal.getMaxDaysOfMonth(month));
 		}
 		
