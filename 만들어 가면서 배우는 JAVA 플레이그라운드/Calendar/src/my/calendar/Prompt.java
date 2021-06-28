2e3330b31c7a7c45803ab1c62eec7a3258e3a962
SeokHo Kim <kim76744@naver.com>
2021-06-28 14:28:47 +0900

    add : Add LeapYear function

--- 만들어 가면서 배우는 JAVA 플레이그라운드/Calendar/src/my/calendar/Prompt.java
+++ 만들어 가면서 배우는 JAVA 플레이그라운드/Calendar/src/my/calendar/Prompt.java
@@ -11,11 +11,15 @@ public class Prompt {
 		Calendar cal = new Calendar();
 		
 		while(true) {
+			
+			System.out.println("년도을 입력하세요");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
 			System.out.println("월을 입력하세요");
-			System.out.print(PROMPT);
+			System.out.print("MONTH>");
 			int month = scanner.nextInt();
 			if (month < 1 || month > 12) break;
-			cal.printCalendar(2017, month);
+			cal.printCalendar(year, month);
 		}
 		
 		System.out.println("반복이 끝났습니다.%n");
