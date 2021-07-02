86a5f475f34f6c68b9aea4bbbd7f0c7fdbee6384
SeokHo Kim <kim76744@naver.com>
2021-07-02 20:45:07 +0900

    add : Add FinallyTest2

---
+++ Java의 정석/ch8/src/chapter/eight/FinallyTest2.java
@@ -0,0 +1,19 @@
+package chapter.eight;
+
+class FinallyTest2 {
+    public static void main(String[] args) {
+        try {
+            startInstall();
+            copyFiles();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            deleteTempFiles();
+        }
+    }
+
+    static void startInstall( ) {    }
+    static void copyFiles( ) {      }
+    static void deleteTempFiles( ){     }
+}
