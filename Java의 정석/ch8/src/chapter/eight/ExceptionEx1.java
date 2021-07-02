d0018af36d02660f442012bb9d3f4d0e8259f8f0
SeokHo Kim <kim76744@naver.com>
2021-07-02 16:05:37 +0900

    add : Add ExceptionEx1

---
+++ Java의 정석/ch8/src/chapter/eight/ExceptionEx1.java
@@ -0,0 +1,17 @@
+package chapter.eight;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try {   } catch (Exception e) { }
+        } catch (Exception e) {
+            try {   } catch (Exception e) { }
+        }
+
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
+}
