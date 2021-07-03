c919bdf139ed073b98328dbe26b9483baada8738
SeokHo Kim <kim76744@naver.com>
2021-07-03 23:00:07 +0900

    add : Add StringEx6

---
+++ Java의 정석/ch9/src/chapter/nine/StringEx6.java
@@ -0,0 +1,18 @@
+package chapter.nine;
+
+import java.io.UnsupportedEncodingException;
+
+class StringEx6 {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String fullName = "Hello.java";
+
+        int index = fullName.indexOf('.');
+
+        String fileName = fullName.substring(0, index);
+
+        String ext = fullName.substring(index + 1);
+
+        System.out.println(fullName + "의 확장자를 제외한 이름은 " + fileName);
+        System.out.println(fullName + "의 확장자는 " + ext);
+    }
+}
