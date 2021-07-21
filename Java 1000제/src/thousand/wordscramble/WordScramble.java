85ac17277e2785bd9f0d7a1ea51a6ddc433e59c8
SeokHo Kim <kim76744@naver.com>
2021-07-21 21:19:17 +0900

    add : Add first commit

---
+++ Java 1000제/src/thousand/wordscramble/WordScramble.java
@@ -0,0 +1,23 @@
+package thousand.wordscramble;
+
+class WordScramble {
+    public static void main(String[] args) {
+        String[] strArr = { "CHANGE", "LOVE", "HOPE", "VIEW"};
+
+        String answer = getAnswer(strArr);
+        String question = getScrambledWord(answer);
+
+        System.out.println("Question:"+question);
+        System.out.println("Answer:"+answer);
+    } // main
+
+    public static String getAnswer(String[] strArr) {
+        // 내용을 완성하세요.
+
+    }
+
+    public static String getScrambledWord(String str) {
+        // 내용을 완성하세요.
+
+    } // scramble(String str)
+}
