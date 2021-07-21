31ce2cb2ad04d3428290d4b95cc0b61d62328851
SeokHo Kim <kim76744@naver.com>
2021-07-21 22:36:58 +0900

    add : Add NEW Question
    while문 완성하기

--- Java 1000제/src/thousand/wordscramble/WordScramble.java
+++ Java 1000제/src/thousand/wordscramble/WordScramble.java
@@ -1,14 +1,28 @@
 package thousand.wordscramble;
+import java.util.*;
 
-class WordScramble {
+class WordScrambleEx2 {
     public static void main(String[] args) {
         String[] strArr = { "CHANGE", "LOVE", "HOPE", "VIEW"};
 
         String answer = getAnswer(strArr);
         String question = getScrambledWord(answer);
 
-        System.out.println("Question:"+question);
-        System.out.println("Answer:"+answer);
+        while(true) {
+            System.out.println("Question :" + question);
+            System.out.print("Your answer is :");
+
+
+            // 1. 화면을 통해 사용자의 입력을 받는다.(Scanner클래스 사용)
+
+            // 2. 사용자가 q 또는 Q를 입력하면 프로그램을 종료한다.
+
+            // 3. 사용자가 정답을 맞출때까지 반복하다가
+
+            //     사용자가 정답을 맞추면, while문을 빠져나간다.
+
+        } // while
+
     } // main
 
     public static String getAnswer(String[] strArr) {
