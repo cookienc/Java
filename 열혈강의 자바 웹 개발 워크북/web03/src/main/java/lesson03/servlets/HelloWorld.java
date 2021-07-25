1dd2e14e1876a0adc253e9d09a1b656654316965
KookieNcream <83010167+kookiedncream@users.noreply.github.com>
2021-07-25 15:30:46 +0900

    add : 서블릿 프로그래밍 실험

---
+++ 열혈강의 자바 웹 개발 워크북/web03/src/main/java/lesson03/servlets/HelloWorld.java
@@ -0,0 +1,46 @@
+package lesson03.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloWorld implements Servlet{
+	
+	ServletConfig config;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init");
+		this.config = config;
+	}
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("service");
+		
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("destroy");
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		System.out.println("getServletConfig");
+		return this.config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		System.out.println("getServletInfo");
+		return "HelloWorld Servlet";
+	}
+
+	
+}
