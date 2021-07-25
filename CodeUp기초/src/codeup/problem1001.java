70e687e6bd425069dacb6eddd8c6b9993286277b
SeokHo Kim <kim76744@naver.com>
2021-07-25 23:15:14 +0900

    add : problem1001

---
+++ CodeUp기초/src/codeup/problem1001.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class problem1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello");
+        bw.flush();
+        bw.close();
+    }
+}
