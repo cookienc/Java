7d3649e6b15ff61bf67ed88824541b8543dc1d1f
SeokHo Kim <kim76744@naver.com>
2021-07-26 21:25:54 +0900

    add : Problem1010

---
+++ CodeUp기초/src/codeup/Problem1010.java
@@ -0,0 +1,14 @@
+package codeup;
+
+
+import java.io.*;
+
+public class Problem1010 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n);
+    }
+}
