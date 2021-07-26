cb4cfaeae14e0efb712cb744526524642a185427
SeokHo Kim <kim76744@naver.com>
2021-07-26 21:12:29 +0900

    add : Problem1008

---
+++ CodeUp기초/src/codeup/Problem1008.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.io.*;
+
+public class Problem1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("\u250C\u252C\u2510\n");
+        bw.write("\u251C\u253C\u2524\n");
+        bw.write("\u2514\u2534\u2518\n");
+        bw.flush();
+        bw.close();
+    }
+}
