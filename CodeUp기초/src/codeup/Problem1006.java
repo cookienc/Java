f79b968cc5403217e0a9f4a5220b3493bb01213a
SeokHo Kim <kim76744@naver.com>
2021-07-26 20:55:06 +0900

    add : Problem1006

---
+++ CodeUp기초/src/codeup/Problem1006.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.io.*;
+
+public class Problem1006 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("!@#$%^&*()");
+        bw.flush();
+        bw.close();
+    }
+}
