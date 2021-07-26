c44a40863483c0f9e033b1598a1d00a13d411488
SeokHo Kim <kim76744@naver.com>
2021-07-26 20:17:29 +0900

    style : rename class name

--- CodeUp기초/src/codeup/Problem1001.java
+++ CodeUp기초/src/codeup/Problem1001.java
@@ -4,10 +4,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class problem1002 {
+public class Problem1001 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("Hello World");
+        bw.write("Hello");
         bw.flush();
         bw.close();
     }
--- CodeUp기초/src/codeup/Problem1002.java
+++ CodeUp기초/src/codeup/Problem1002.java
@@ -4,10 +4,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class problem1001 {
+public class Problem1002 {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("Hello");
+        bw.write("Hello World");
         bw.flush();
         bw.close();
     }
