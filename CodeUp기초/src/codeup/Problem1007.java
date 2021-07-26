90a706609424bac09f586c1e1af1eb1c6232674e
SeokHo Kim <kim76744@naver.com>
2021-07-26 21:06:33 +0900

    add : Problem1007

---
+++ CodeUp기초/src/codeup/Problem1007.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.io.*;
+
+public class Problem1007 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("C\\Download\\hello.cpp");
+        bw.flush();
+        bw.close();
+    }
+}
