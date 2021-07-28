582e70764f966ec46c3f0b94568f448575a250d5
SeokHo Kim <kim76744@naver.com>
2021-07-28 22:08:03 +0900

    add : Problem1011

---
+++ CodeUp기초/src/codeup/Problem1011.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1011 {
+    public static void main(String[] args) {
+        String str;
+        char ch;
+        Scanner sc = new Scanner(System.in);
+        ch = sc.nextLine().charAt(0);
+        System.out.println(ch);
+    }
+}
