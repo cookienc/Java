65bb8fb370ecb14d9fa27faa6f9e65c03598497e
SeokHo Kim <kim76744@naver.com>
2021-07-29 16:52:23 +0900

    add : Problem1020

---
+++ CodeUp기초/src/codeup/Problem1020.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1020 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] number = sc.nextLine().split("-");
+
+        System.out.println(number[0] + number[1] + "");
+    }
+}
