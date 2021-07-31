970c198d97ed1262c77b8e53ebb070c9219b149a
SeokHo Kim <kim76744@naver.com>
2021-07-31 19:47:40 +0900

    add : Problem1029

---
+++ CodeUp기초/src/codeup/Problem1029.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1029 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num = scanner.nextDouble();
+
+        System.out.printf("%.11f", num);
+    }
+}
