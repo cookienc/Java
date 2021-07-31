3f5e03c3937b35e553db75193b2e28c8e1abfabc
SeokHo Kim <kim76744@naver.com>
2021-07-31 17:38:10 +0900

    add : Problem1021

---
+++ CodeUp기초/src/codeup/Problem1021.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String strings = scanner.nextLine();
+
+        System.out.println(strings);
+    }
+}
