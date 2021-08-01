abf3bf7bc66a9ebdc278b824abcf6885f78aefa1
SeokHo Kim <kim76744@naver.com>
2021-08-01 19:25:30 +0900

    add : Problem1039

---
+++ CodeUp기초/src/codeup/Problem1039.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1039 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long num1 = scanner.nextLong();
+        long num2 = scanner.nextLong();
+
+        System.out.println(num1 + num2);
+    }
+}
