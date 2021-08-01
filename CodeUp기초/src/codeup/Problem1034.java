03a4d02c1c75ca7247dba687fc3f065cf0be1a40
SeokHo Kim <kim76744@naver.com>
2021-08-01 19:04:35 +0900

    add : Problem1034

---
+++ CodeUp기초/src/codeup/Problem1034.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1034 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt(8);
+
+        System.out.printf("%d", num);
+    }
+}
