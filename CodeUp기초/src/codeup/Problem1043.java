a273de164b16314d309525ff563cfaa480b1e066
SeokHo Kim <kim76744@naver.com>
2021-08-02 13:37:17 +0900

    add : Problem1043

---
+++ CodeUp기초/src/codeup/Problem1043.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1043 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+
+        System.out.println(a % b);
+
+    }
+}
