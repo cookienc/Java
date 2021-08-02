aae34d0e4da15bf1a6ff5dd0654961bab8b15ae5
SeokHo Kim <kim76744@naver.com>
2021-08-02 13:38:42 +0900

    add : Problem1044

---
+++ CodeUp기초/src/codeup/Problem1044.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1044 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long num = scanner.nextLong();
+
+        System.out.println(num + 1);
+
+    }
+}
