682951a151f937d469951feefd81872ba801dc84
SeokHo Kim <kim76744@naver.com>
2021-08-03 14:30:53 +0900

    add : Problem1057

---
+++ CodeUp기초/src/codeup/Problem1057.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1057 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println((a == b) ? 1 : 0);
+    }
+}
