3f4c2d5ce8ef4cc38a3d3dca389628730779079b
SeokHo Kim <kim76744@naver.com>
2021-08-03 13:49:38 +0900

    add : Problem1053

---
+++ CodeUp기초/src/codeup/Problem1053.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        System.out.println(a == 1 ? 0 : 1);
+    }
+}
