a9f7376374776f56bd63445227158d7aa44f3660
SeokHo Kim <kim76744@naver.com>
2021-08-05 18:48:20 +0900

    add : Problem1073

---
+++ CodeUp기초/src/codeup/Problem1073.java
@@ -0,0 +1,20 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Problem1073 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            int a = scanner.nextInt();
+
+            if (a == 0) {
+                break;
+            }
+
+            System.out.println(a);
+        }
+    }
+}
