109cc5edfb684043704e5b9408802c48bd40b97a
SeokHo Kim <kim76744@naver.com>
2021-08-10 22:04:50 +0900

    feat : SEQUENCE 전략 확인

--- JPA프로그래밍/src/main/java/hellojpa/Member.java
+++ JPA프로그래밍/src/main/java/hellojpa/Member.java
@@ -3,20 +3,22 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator")
+    private Long id;
 
     @Column(name = "name")
     private String username;
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
