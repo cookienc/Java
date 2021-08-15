7b8fc91bbec4aa47dc9d1eb37a93bf4b4bfa5e24
SeokHo Kim <kim76744@naver.com>
2021-08-15 17:13:21 +0900

    feat : 프록시 개념

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.lang.model.SourceVersion;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,19 +16,24 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("user");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("m1 = " + refMember.getClass());
+
+            System.out.println("refMember = " + refMember.getUsername());
+
+            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
