4c0419f8c90e20e698fb8a156233640621b81ad3
SeokHo Kim <kim76744@naver.com>
2021-08-16 15:14:42 +0900

    add : 값 타입 컬렉션 사용

--- JPA프로그래밍/src/main/java/hellojpa/Member.java
+++ JPA프로그래밍/src/main/java/hellojpa/Member.java
@@ -2,7 +2,10 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -26,6 +29,19 @@ public class Member {
     //주소
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+    @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressesHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
