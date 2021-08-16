af142f0143ece37dc4ddf6808edd73fc77964b62
SeokHo Kim <kim76744@naver.com>
2021-08-16 14:19:05 +0900

    add : 임베디드 타입의 부작용(side effect)

--- JPA프로그래밍/src/main/java/hellojpa/Member.java
+++ JPA프로그래밍/src/main/java/hellojpa/Member.java
@@ -27,17 +27,6 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name="city",
-                    column=@Column(name = "WORK_CITY")),
-            @AttributeOverride(name="street",
-                    column=@Column(name = "WORK_STREET")),
-            @AttributeOverride(name="zipcode",
-                    column=@Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
-
     public Long getId() {
         return id;
     }
@@ -76,14 +65,6 @@ public class Member {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
-    }
-
-    public Address getWorkAddress() {
-        return workAddress;
-    }
-
-    public void setWorkAddress(Address workAddress) {
-        this.workAddress = workAddress;
     }
 }
 
