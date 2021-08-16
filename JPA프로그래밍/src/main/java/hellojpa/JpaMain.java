f87648964a5372d5fb72cdba53d9d479d3d78545
SeokHo Kim <kim76744@naver.com>
2021-08-16 15:22:03 +0900

    add : 값 타입 저장 예제

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,18 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city", "street", "10");
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
-            em.persist(member);
+            member.setHomeAddress(new Address("homeCity", "street", "1000"));
 
-            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
-            member.setHomeAddress(newAddress);
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressesHistory().add(new Address("old1", "street", "1000"));
+            member.getAddressesHistory().add(new Address("old2", "street", "1000"));
+
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
