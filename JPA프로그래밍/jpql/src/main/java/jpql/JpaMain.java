ef12e16d6f89f708666d8705f9aa9e33fd4aa645
SeokHo Kim <kim76744@naver.com>
2021-08-17 16:14:03 +0900

    add : 엔티티 프로젝션

--- JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
+++ JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
@@ -19,10 +19,14 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            Member singleResult = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("singleResult.getUsername() = " + singleResult.getUsername());
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList();
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
 
             tx.commit();
         } catch (Exception e) {
