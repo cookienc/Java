14bae33e07f4b1a116ec608e7bdb150374d49296
SeokHo Kim <kim76744@naver.com>
2021-08-17 15:11:03 +0900

    add : NativeSQL 추가

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -20,13 +20,7 @@ public class JpaMain {
 
         try {
 
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-
-            Root<Member> m = query.from(Member.class);
-
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-            List<Member> resultList = em.createQuery(cq)
+            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
                     .getResultList();
 
 
