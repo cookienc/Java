078a6d27c966c5615ca859abd818e1bd2d5ffefb
SeokHo Kim <kim76744@naver.com>
2021-08-17 15:59:49 +0900

    add : getResultList()

--- JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
+++ JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,7 +20,10 @@ public class JpaMain {
             em.persist(member);
 
             TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            Query query1 = em.createQuery("select m.username, m.age from Member m");
+            List<Member> resultList = query.getResultList();
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
