11c17bf8d1377537309cdc107bf17c69afd80ecd
SeokHo Kim <kim76744@naver.com>
2021-08-17 18:01:51 +0900

    add : 세타 조인 테스트

--- JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
+++ JPA프로그래밍/jpql/src/main/java/jpql/JpaMain.java
@@ -30,7 +30,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left outer join m.team t";
+            String query = "select m from Member m, Team t where m.username = t.name";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
