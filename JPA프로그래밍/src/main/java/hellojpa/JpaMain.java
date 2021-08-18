d228e844b0a165bd8604f36b7934c8b596f53859
SeokHo Kim <kim76744@naver.com>
2021-08-18 22:59:43 +0900

    add : 상태필드

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -18,11 +18,26 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
+        em.flush();
+        em.clear();
+
         try {
 
-            em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME from MEMBER")
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
+
+            String query = "select m.username from Member m";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
+            for (String s : result) {
+                System.out.println("s = " + s);
+            }
 
             tx.commit();
         } catch (Exception e) {
