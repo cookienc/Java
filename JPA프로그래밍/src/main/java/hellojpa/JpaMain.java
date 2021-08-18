481a26a21f2a83fcd61e4c35773abf8296732533
SeokHo Kim <kim76744@naver.com>
2021-08-18 23:16:03 +0900

    add : 컬렉션 값 연관 경로(from으로 별칭을 얻어서 탐색하기)

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -1,14 +1,8 @@
 package hellojpa;
 
-import javax.lang.model.SourceVersion;
 import javax.persistence.*;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -24,24 +18,30 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
-            String query = "select t.members from Team t";
-            Collection result = em.createQuery(query, Collection.class)
+            em.flush();
+            em.clear();
+
+            String query = "select m.username from Team t join t.members m";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (Object o : result) {
-                System.out.println("o = " + o);
-                
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
             e.printStackTrace();
