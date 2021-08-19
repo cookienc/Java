85d4bf6cef4961be4bb6c81ddeb0f1c214deb7c1
SeokHo Kim <kim76744@naver.com>
2021-08-19 14:28:07 +0900

    add : 컬렉션 페치 조인
    중복 출력 발생 확인

--- JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
+++ JPA프로그래밍/src/main/java/hellojpa/JpaMain.java
@@ -44,15 +44,13 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m From Member m join fetch m.team";
-            List<Member> result = em.createQuery(query, Member.class)
+            String query = "select t From Team t join fetch t.members";
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-                //회원1, 팀A(SQL)
-                //회원2, 팀A(1차캐시)
-                //회원3, 팀B(SQL)
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|members = " + team.getMembers().size());
+
             }
 
             tx.commit();
