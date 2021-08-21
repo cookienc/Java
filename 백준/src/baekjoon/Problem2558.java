794e61d00a8058c237d1ebbe63d6dd250da6f083
SeokHo Kim <kim76744@naver.com>
2021-08-21 09:50:31 +0900

    add : Problem2558

---
+++ 백준/src/baekjoon/Problem2558.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println(a + b);
+    }
+}
