6007b2f53fdd9f1eee5a2b8255789a7fac4e7838
SeokHo Kim <kim76744@naver.com>
2021-08-21 10:25:34 +0900

    add : Problem10950
    다른 방법으로 풀이

--- 백준/src/baekjoon/Problem10950.java
+++ 백준/src/baekjoon/Problem10950.java
@@ -6,8 +6,7 @@ import java.util.StringTokenizer;
 public class Problem10950 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder stringBuilder = new StringBuilder();
         int num = Integer.parseInt(bufferedReader.readLine());
 
         for (int i = 0; i < num; i++) {
@@ -16,8 +15,9 @@ public class Problem10950 {
             int a = Integer.parseInt(stringTokenizer.nextToken());
             int b = Integer.parseInt(stringTokenizer.nextToken());
 
-            bufferedWriter.write(a + b + "\n");
+            stringBuilder.append(a + b);
+            stringBuilder.append("\n");
         }
-        bufferedWriter.flush();
+        System.out.println(stringBuilder);
     }
 }
