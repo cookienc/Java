9e5e25aaa19eb4ac69403823b06b21714540ddbf
SeokHo Kim <kim76744@naver.com>
2021-09-13 15:30:08 +0900

    fix : Problem2225

--- 백준/src/baekjoon/Problem2225.java
+++ 백준/src/baekjoon/Problem2225.java
@@ -6,37 +6,39 @@ import java.util.StringTokenizer;
 
 public class Problem2225 {
 
-    private static int[][] dp;
+    private static int[][] dp = new int[201][201];
 
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
-        int k = Integer.parseInt(stringTokenizer.nextToken());
         int n = Integer.parseInt(stringTokenizer.nextToken());
-
-        if (k < 1 || k > 200) {
-            throw new Exception("오류");
-        }
+        int k = Integer.parseInt(stringTokenizer.nextToken());
 
         if (n < 1 || n > 200) {
             throw new Exception("오류");
         }
 
-        dp = new int[k + 1][n + 1];
+        if (k < 1 || k > 200) {
+            throw new Exception("오류");
+        }
 
-        for (int i = 1; i <= k; i++) {
-            dp[i][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            dp[i][1] = 1;
         }
 
         for (int i = 1; i <= k; i++) {
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j - 1]) % 1000000000;
+            dp[1][i] = i;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= k; j++) {
+                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % 1000000000;
             }
         }
 
-        System.out.println(dp[k][n]);
+        System.out.println(dp[n][k]);
 
     }
 }
