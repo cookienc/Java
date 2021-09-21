24a820cdd9002e4603d0853c2c9aedcce3c53e03
SeokHo Kim <kim76744@naver.com>
2021-09-21 16:29:45 +0900

    add : Problem10989

---
+++ 백준/src/baekjoon/Problem10989.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Problem10989 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int num = Integer.parseInt(bufferedReader.readLine());
+
+        if (num < 1 || num > 10000000) {
+            throw new Exception("오류");
+        }
+
+        int[] array = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            array[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        Arrays.sort(array);
+
+        for (int i : array) {
+            stringBuilder.append(i)
+                    .append("\n");
+        }
+
+        System.out.println(stringBuilder);
+
+    }
+}
