4232b455cac7284ddb8c510c709f2f0bc27bf793
SeokHo Kim <kim76744@naver.com>
2021-10-27 22:31:51 +0900

    add : Problem2089

---
+++ 백준/src/baekjoon/Problem2089.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem2089 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bufferedReader.readLine());
+
+        if (num == 0) {
+            System.out.println(num);
+            System.exit(0);
+        }
+
+        String answer = "";
+
+        while (num != 0) {
+            if (num % -2 == -1) {
+                answer = ((num % -2) + 2) + answer;
+                num = (num / -2) + 1;
+            } else {
+                answer = (num % -2) + answer;
+                num /= -2;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
