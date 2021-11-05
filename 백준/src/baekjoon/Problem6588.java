6e1521e1c19f477914ad1c7a452c81e2395fd202
kookiedncream <kim76744@naver.com>
2021-11-05 13:58:30 +0900

    refactor : Problem6588
    미리 소수를 만들어 놓는것으로 변경

--- 백준/src/baekjoon/Problem6588.java
+++ 백준/src/baekjoon/Problem6588.java
@@ -6,69 +6,41 @@ import java.io.InputStreamReader;
 
 public class Problem6588 {
 
-    private static boolean[] prime;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder stringBuilder = new StringBuilder();
 
-        int n = Integer.parseInt(bufferedReader.readLine());
+        boolean[] isNotPrime = new boolean[1000001];
 
-        while (n != 0) {
-
-            findPrime(n);
-
-            boolean exit = false;
-
-            for (int i = prime.length - 2; i > prime.length / 2; i--) {
-                for (int j = 2; j <= i; j++) {
-                    if ((!prime[i]) && (!prime[j]) && (n == (i + j))) {
-                        stringBuilder.append(n)
-                                .append(" = ")
-                                .append(j)
-                                .append(" + ")
-                                .append(i)
-                                .append("\n");
-                        exit = true;
-                        break;
-                    }
+        for (int i = 2; i <= 1000000; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * 2; j <= 1000000; j += i) {
+                    isNotPrime[j] = true;
                 }
-                if (exit) {
+            }
+        }
+
+        while(true) {
+            int number = Integer.parseInt(bufferedReader.readLine());
+            boolean isExists = false;
+
+            if(number == 0) {
+                System.out.println(stringBuilder);
+
+                return;
+            }
+
+            for (int i = 3; i <= number; i += 2) {
+                if (!isNotPrime[i] && !isNotPrime[number - i]) {
+                    stringBuilder.append(number + " = " + i + " + " + (number - i)).append("\n");
+                    isExists = true;
                     break;
                 }
-
-                if (i == prime.length + 1) {
-                    stringBuilder.append("Goldbach's conjecture is wrong.")
-                            .append("\n");
-                }
-            }
-            n = Integer.parseInt(bufferedReader.readLine());
-        }
-
-        System.out.println(stringBuilder);
-    }
-
-    public static void findPrime(int n) {
-
-        prime = new boolean[n + 1];
-
-        if (n < 2) {
-            return;
-        }
-
-        prime[0] = true;
-        prime[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-
-            if (prime[i]) {
-                continue;
             }
 
-            for (int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+            if (!isExists) {
+                stringBuilder.append("Goldbach's conjecture is wrong.").append("\n");
             }
         }
     }
