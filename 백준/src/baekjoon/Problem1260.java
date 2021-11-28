65497b4ab4b9a0069699995cb4e2bdf6787e9eb6
kookiedncream <kim76744@naver.com>
2021-11-28 20:24:34 +0900

    refactor : dfs 리펙토링

--- 백준/src/baekjoon/Problem1260.java
+++ 백준/src/baekjoon/Problem1260.java
@@ -1,111 +1,82 @@
 package baekjoon;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Problem1260 {
 
-    static int N;
-    static int M;
-    static int V;
+    private static List<Integer>[] map;
+    private static List<Integer> dfsList;
+    private static boolean[] isVisited;
+    private static int n;
+    private static int m;
+    private static int v;
 
-    static List<Integer>[] line;
-
-    static int index = 0;
-    static int[] visited = new int[1001];
-    static boolean isEnd = false;
-
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 
-        String input = bufferedReader.readLine();
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        m = Integer.parseInt(stringTokenizer.nextToken());
+        v = Integer.parseInt(stringTokenizer.nextToken());
 
-        StringTokenizer stringTokenizer = new StringTokenizer(input);
-        N = Integer.parseInt(stringTokenizer.nextToken());
-        M = Integer.parseInt(stringTokenizer.nextToken());
-        V = Integer.parseInt(stringTokenizer.nextToken());
+        map = new ArrayList[n + 1];
+        dfsList = new ArrayList<>();
+        isVisited = new boolean[n + 1];
 
-        line = new ArrayList[N + 1];
 
-        for (int i = 1; i <= N; i++)
-        {
-            line[i] = new ArrayList<Integer>();
+        initializeList(bufferedReader);
+
+        sortList();
+
+        dfs(v);
+
+        for (Integer integer : dfsList) {
+            System.out.print(integer + " ");
         }
 
-        String[] inputs = new String[2];
 
-        for (int i = 1; i <= M; i++)
-        {
-            input = bufferedReader.readLine();
-            inputs = input.split(" ");
-            line[Integer.parseInt(inputs[0])].add(Integer.parseInt(inputs[1]));
-            line[Integer.parseInt(inputs[1])].add(Integer.parseInt(inputs[0]));
-        }
-
-        for (int i = 1; i <= N; i++)
-        {
-            Collections.sort(line[i]);
-        }
-
-        // DFS
-        visited[V] = 1;
-
-        System.out.print(V + " ");
-
-        index++;
-
-        dfs(V);
-
-        for (int i = 1; i <= N; i++)
-        {
-            visited[i] = 0;
-        }
-
-        System.out.println();
-
-        // BFS
-        bfs(V);
-        System.out.println();
     }
 
-    private static void bfs(int node)
-    {
-        Queue<Integer> queue = new LinkedList<Integer>();
+    private static void dfs(int num) {
 
-        queue.add(node);
-        visited[node] = 1;
-        index = 1;
+        if (isVisited[num]) {
+            return;
+        }
 
-        while (queue.isEmpty() == false)
-        {
-            node = queue.remove();
-            System.out.print(node + " ");
-            for (int next : line[node])
-            {
-                if (visited[next] == 0)
-                {
-                    index++;
-                    visited[next] = 1;
-                    queue.add(next);
-                }
+        isVisited[num] = true;
+        dfsList.add(num);
+
+        for (int y : map[num]) {
+            if (!isVisited[y]) {
+                dfs(y);
             }
         }
     }
 
-    private static void dfs(int node)
-    {
-        for (int next : line[node])
-        {
-            if (visited[next] == 0)
-            {
-                System.out.print(next + " ");
-                index++;
-                visited[next] = 1;
-                dfs(next);
-            }
+    private static void sortList() {
+        for (int i = 1; i < n + 1; i++) {
+            Collections.sort(map[i]);
+        }
+    }
+
+    private static void initializeList(BufferedReader bufferedReader) throws IOException {
+        StringTokenizer stringTokenizer;
+        for (int i = 1; i < n + 1; i++) {
+            map[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 1; i < m + 1; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+
+            map[a].add(b);
+            map[b].add(a);
         }
     }
 }
