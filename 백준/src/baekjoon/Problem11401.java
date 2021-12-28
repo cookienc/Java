71cfd265c55021002aaf3d7904a2540c8a9319b8
kookiedncream <kim76744@naver.com>
2021-12-28 14:11:06 +0900

    add: Problem11401
    페르마의 소정리 이용

--- 백준/src/baekjoon/Problem11401.java
+++ 백준/src/baekjoon/Problem11401.java
@@ -5,9 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * 출처: https://www.acmicpc.net/problem/11401
+ * 참고: https://st-lab.tistory.com/241?category=922819
+ */
 public class Problem11401 {
 
-	private static int num = 1000000007;
+	private static long[] factorial;
+	private static final long num = 1000000007L;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
@@ -15,21 +21,44 @@ public class Problem11401 {
 		int n = Integer.parseInt(stringTokenizer.nextToken());
 		int k = Integer.parseInt(stringTokenizer.nextToken());
 
-		System.out.println(combination(n, k));
+		factorial = new long[n + 1];
+
+		makeFactorial(n);
+
+		long a = factorial[n];
+		long b = (factorial[k] * factorial[n - k]) % num;
+
+		long answer = (a % num) * (pow(b, num - 2) % num) % num;
+
+		System.out.println(answer);
 	}
 
-	private static int combination(int n, int k) {
+	private static void makeFactorial(int n) {
 
-		if (k == 0 || n == 1 || k == n) {
-			return 1;
+		factorial[0] = 1L;
+		factorial[1] = 1L;
+
+		if (n < 2) {
+			return;
 		}
+
+		for (int i = 2; i <= n; i++) {
+			factorial[i] = (factorial[i - 1] * i) % num;
+		}
+	}
+
+	private static long pow(long n, long k) {
 
 		if (k == 1) {
 			return n % num;
 		}
 
-		int temp = combination(n - 1, k - 1) % num + combination(n - 1, k) % num;
+		long temp = pow(n, k / 2);
 
-		return temp % num;
+		if (k % 2 == 1) {
+			return (temp * temp % num) * n % num;
+		}
+
+		return temp * temp % num;
 	}
 }
