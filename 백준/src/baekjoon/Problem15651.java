e2f418d56d36638c7336a63c6403cf28e0493a3d
kookiedncream <kim76744@naver.com>
2022-01-07 21:08:28 +0900

    feat: Problem15651

---
+++ 백준/src/baekjoon/Problem15651.java
@@ -0,0 +1,48 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/15651
+ */
+public class Problem15651 {
+
+	private static int N;
+	private static int M;
+	private static int[] array;
+	private static StringBuilder stringBuilder;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+		N = Integer.parseInt(stringTokenizer.nextToken());
+		M = Integer.parseInt(stringTokenizer.nextToken());
+
+		array = new int[M];
+
+		stringBuilder = new StringBuilder();
+		dfs(0);
+
+		System.out.println(stringBuilder);
+	}
+
+	private static void dfs(int depth) {
+		if (depth == M) {
+			for (int i : array) {
+				stringBuilder.append(i)
+						.append(" ");
+			}
+			stringBuilder.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			array[depth] = i;
+			dfs(depth + 1);
+		}
+	}
+}
