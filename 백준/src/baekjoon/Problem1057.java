b6c8a7f2d46f68e3c18cdf95cea4adc87c557b93
kookiedncream <kim76744@naver.com>
2022-01-14 21:53:51 +0900

    feat: Problem1057

--- 백준/src/baekjoon/Problem1057.java
+++ 백준/src/baekjoon/Problem1057.java
@@ -5,9 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 출처: https://www.acmicpc.net/problem/1057
- */
 public class Problem1057 {
 
 	private static int N;
@@ -22,30 +19,14 @@ public class Problem1057 {
 		a = Integer.parseInt(stringTokenizer.nextToken());
 		b = Integer.parseInt(stringTokenizer.nextToken());
 
-		boolean[] array = new boolean[N];
-		int count = 1;
+		int count = 0;
 		
-		while (true) {
-
-			if (b == N || (N % 2 == 1)) {
-				count++;
-				nextRound();
-			}
-
-			if (Math.abs(a - b) == 1) {
-				if ((a % 2 == 1) && (b % 2 == 0)) {
-					break;
-				}
-			}
-			
+		while (a != b) {
+			a = (a + 1) / 2;
+			b = (b + 1) / 2;
 			count++;
-			nextRound();
 		}
 		System.out.println(count);
 	}
 
-	private static void nextRound() {
-		a = a / 2 + a % 2;
-		b = b / 2 + b % 2;
-	}
 }
