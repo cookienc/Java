7ff71d02ea6ebb9f7e79939e77e45739e6605b6e
kookiedncream <kim76744@naver.com>
2022-01-19 22:14:55 +0900

    refactor: Problem14888
    간단하게 변경

--- 백준/src/baekjoon/Problem14888.java
+++ 백준/src/baekjoon/Problem14888.java
@@ -3,119 +3,76 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
  * 출처: https://www.acmicpc.net/problem/14888
+ * 참고: https://st-lab.tistory.com/121
  */
 public class Problem14888 {
 
-	private static int[] number;
-	private static int[] param;
-	private static boolean[] isVisited;
-	private static List<Character> symbol;
-	private static char[] symbolArr;
-	private static int end;
-	private static int max = Integer.MIN_VALUE;
-	private static int min = Integer.MAX_VALUE;
-	private static int count;
+	public static int MAX = Integer.MIN_VALUE;
+	public static int MIN = Integer.MAX_VALUE;
+	public static int[] operator = new int[4];
+	public static int[] number;
+	public static int N;
 
 	public static void main(String[] args) throws IOException {
+
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bufferedReader.readLine());
 
+		N = Integer.parseInt(bufferedReader.readLine());
 		number = new int[N];
-		param = new int[4];
-
 
 		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 		for (int i = 0; i < N; i++) {
 			number[i] = Integer.parseInt(stringTokenizer.nextToken());
 		}
 
-		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-		count = 0;
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
 		for (int i = 0; i < 4; i++) {
-			param[i] = Integer.parseInt(stringTokenizer.nextToken());
-			count += param[i];
+			operator[i] = Integer.parseInt(stringTokenizer.nextToken());
 		}
 
-		isVisited = new boolean[count];
-		symbol = new ArrayList<>();
+		dfs(number[0], 1);
 
-		for (int i = 0; i < 4; i++) {
-			int num = param[i];
-			for (int j = 0; j < num; j++) {
-				switch (i) {
-					case 0:
-						symbol.add('+');
-						continue;
-					case 1:
-						symbol.add('-');
-						continue;
-					case 2:
-						symbol.add('x');
-						continue;
-					case 3:
-						symbol.add('/');
-						continue;
-				}
-			}
-		}
-
-		end = N - 1;
-		symbolArr = new char[end];
-
-		dfs(0);
-
-		System.out.println(max);
-		System.out.println(min);
+		System.out.println(MAX);
+		System.out.println(MIN);
 
 	}
 
-	private static void dfs(int depth) {
-		if (depth == end) {
-			int sum = number[0];
-
-			for (int i = 0; i < end; i++) {
-				char check = symbolArr[i];
-
-				if (check == '+') {
-					sum += number[i + 1];
-					continue;
-				}
-
-				if (check == '-') {
-					sum -= number[i + 1];
-					continue;
-				}
-
-				if (check == 'x') {
-					sum *= number[i + 1];
-					continue;
-				}
-
-				if (check == '/') {
-					sum /= number[i + 1];
-					continue;
-				}
-			}
-
-			max = Math.max(sum, max);
-			min = Math.min(sum, min);
-
+	public static void dfs(int num, int index) {
+		if (index == N) {
+			MAX = Math.max(MAX, num);
+			MIN = Math.min(MIN, num);
 			return;
 		}
 
-		for (int i = 0; i < count; i++) {
-			if (!isVisited[i]) {
-				isVisited[i] = true;
-				symbolArr[depth] = symbol.get(i);
-				dfs(depth + 1);
-				isVisited[i] = false;
+		for (int i = 0; i < 4; i++) {
+
+			if (operator[i] > 0) {
+
+				operator[i]--;
+
+				switch (i) {
+
+					case 0:
+						dfs(num + number[index], index + 1);
+						break;
+					case 1:
+						dfs(num - number[index], index + 1);
+						break;
+					case 2:
+						dfs(num * number[index], index + 1);
+						break;
+					case 3:
+						dfs(num / number[index], index + 1);
+						break;
+
+				}
+				operator[i]++;
 			}
 		}
 	}
+
 }
