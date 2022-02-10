60417f4aa9d1f51579c8a6bc314d6c3fadc03645
kookiedncream <kim76744@naver.com>
2022-02-10 21:55:06 +0900

    feat: Problem1001

---
+++ 백준/src/baekjoon/Problem1001.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/1001
+ */
+public class Problem1001 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		System.out.println(A - B);
+	}
+}
