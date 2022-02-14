9f0d36459c5da8a42166072505808886f08e628e
kookiedncream <kim76744@naver.com>
2022-02-14 10:47:28 +0900

    feat: Problem10171

---
+++ 백준/src/baekjoon/Problem10171.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/10171
+ */
+public class Problem10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
