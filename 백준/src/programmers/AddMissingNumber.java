fef1e57db1344bbf59e9a81a81e11878d4fb8b4f
kookiedncream <kim76744@naver.com>
2022-03-18 11:07:59 +0900

    feat: AddMissingNumber

---
+++ 백준/src/programmers/AddMissingNumber.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class AddMissingNumber {
+	class Solution {
+		public int solution(int[] numbers) {
+			int answer = 45;
+			for (int i : numbers) {
+				answer -= i;
+			}
+			return answer;
+		}
+	}
+}
