dadd386c07758303dfd7833f5d04d226caece01b
kookiedncream <kim76744@naver.com>
2022-03-20 10:08:52 +0900

    feat: OneTwoFourCountry

---
+++ 백준/src/programmers/OneTwoFourCountry.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class OneTwoFourCountry {
+	class Solution {
+		public String solution(int n) {
+			String[] num = {"4","1","2"};
+			String answer = "";
+
+			while(n > 0) {
+				answer = num[n % 3] + answer;
+				n = (n - 1) / 3;
+			}
+			return answer;
+		}
+	}
+}
