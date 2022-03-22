287b785bf326884d4190c2a9959280ed92fbadcc
kookiedncream <kim76744@naver.com>
2022-03-22 10:04:21 +0900

    feat: KthNumber

---
+++ 백준/src/programmers/KthNumber.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class KthNumber {
+	static class Solution {
+		public int[] solution(int[] array, int[][] commands) {
+
+			int[] answer = new int[commands.length];
+
+			for (int i = 0; i < commands.length; i++) {
+				int start = commands[i][0] - 1;
+				int end = commands[i][1] - 1;
+				int index = commands[i][2] - 1;
+
+				List<Integer> list = new ArrayList<>();
+				for (int j = start; j <= end; j++) {
+					list.add(array[j]);
+				}
+				list.sort(Comparator.naturalOrder());
+				answer[i] = list.get(index);
+
+			}
+			return answer;
+		}
+	}
+}
