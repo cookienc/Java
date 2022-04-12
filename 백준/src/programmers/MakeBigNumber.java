3aab1581acbdfa9996c0bf2229c3570c5225f60e
kookiedncream <kim76744@naver.com>
2022-04-12 09:31:59 +0900

    feat: MakeBigNumber

---
+++ 백준/src/programmers/MakeBigNumber.java
@@ -0,0 +1,26 @@
+package programmers;
+
+/**
+ * 출처: https://programmers.co.kr/learn/courses/30/lessons/42883
+ */
+public class MakeBigNumber {
+	class Solution {
+
+		public String solution(String number, int k) {
+			StringBuilder sb = new StringBuilder();
+			int index = 0;
+			int max = 0;
+			for(int i = 0; i < number.length() - k; i++) {
+				max = 0;
+				for(int j = index; j <= k + i; j++) {
+					if(max < number.charAt(j) - '0') {
+						max = number.charAt(j) - '0';
+						index = j + 1;
+					}
+				}
+				sb.append(max);
+			}
+			return sb.toString();
+		}
+	}
+}
