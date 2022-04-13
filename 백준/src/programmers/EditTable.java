243bc6ab194b1c34e6d4db56aea71825852e85cf
kookiedncream <kim76744@naver.com>
2022-04-13 21:18:38 +0900

    feat: EditTable
    효율성 통과 -> Stack만 사용해도 가능

--- 백준/src/programmers/EditTable.java
+++ 백준/src/programmers/EditTable.java
@@ -1,88 +1,57 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Stack;
 
 public class EditTable {
 	static class Solution {
 
-		private static boolean[] isDeleted;
-		private static List<Integer> list;
-		private static int index;
-		private static Stack<Command> previous;
+		private static Stack<Integer> previous;
 
 		public String solution(int n, int k, String[] cmd) {
 
-			isDeleted = new boolean[n];
-			list = new ArrayList<>();
-			index = k;
 			previous = new Stack<>();
-			Arrays.fill(isDeleted, false);
+			int size = n;
 
-			for (int i = 0; i < n; i++) {
-				list.add(i);
-			}
-
-			for (int i = 0; i < cmd.length; i++) {
-				String[] word = cmd[i].split(" ");
+			for (String c : cmd) {
+				String[] word = c.split(" ");
 
 				if ("D".equals(word[0])) {
-					index += Integer.parseInt(word[1]);
+					k += Integer.parseInt(word[1]);
 					continue;
 				}
 
 				if ("U".equals(word[0])) {
-					index -= Integer.parseInt(word[1]);
+					k -= Integer.parseInt(word[1]);
 					continue;
 				}
 
 				if ("C".equals(word[0])) {
-					previous.push(new Command(index, list.get(index)));
-					list.remove(index);
+					previous.push(k);
 
-					if (index == list.size()) {
-						index = list.size() - 1;
+					if (k == size - 1) {
+						k = size - 2;
 					}
+					size--;
 					continue;
 				}
 
 				//Z일경우
-				Command p = previous.pop();
+				Integer i = previous.pop();
 
-				if (p.index <= index) {
-					index++;
+				if (i <= k) {
+					k++;
 				}
-
-				list.add(p.index, p.value);
-			}
-
-			for (Integer i : list) {
-				isDeleted[i] = true;
+				size++;
 			}
 
 			StringBuilder sb = new StringBuilder();
-			for (boolean b : isDeleted) {
-				if (b) {
-					sb.append("O");
-					continue;
-				}
-				sb.append("X");
+			for(int i = 0; i < size; i++) {
+				sb.append('O');
 			}
-
+			while(!previous.isEmpty()) {
+				sb.insert(previous.pop().intValue(), 'X');
+			}
 			return sb.toString();
-		}
-
-		private static class Command {
-
-			int index;
-			int value;
-
-			public Command(int index, int value) {
-				this.index = index;
-				this.value = value;
-			}
 		}
 	}
 }
