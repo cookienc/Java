9165c773a9fbfc69495aac3014d336a9b3c477d0
kookiedncream <kim76744@naver.com>
2022-05-12 11:53:05 +0900

    feat: Problem2098
    list를 배열로 바꿈

--- 백준/src/baekjoon/Problem2098.java
+++ 백준/src/baekjoon/Problem2098.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 /**
@@ -13,11 +11,10 @@ import java.util.StringTokenizer;
  */
 public class Problem2098 {
 
-	private static List<List<Node>> map;
 	private static int[][] dp;
+	private static int[][] map;
 	private static int N;
 	private static int start;
-	private static int answer = Integer.MAX_VALUE;
 	private static int INF = 11_000_000;
 
 	public static void main(String[] args) throws IOException {
@@ -25,73 +22,47 @@ public class Problem2098 {
 
 		N = Integer.parseInt(br.readLine());
 
-		initializeMap(N);
-		inputData(br, N);
-
+		map = new int[N][N];
 		dp = new int[N][(1 << N) - 1];
 
 		for (int i = 0; i < N; i++) {
 			Arrays.fill(dp[i], INF);
 		}
 
-		System.out.println(dfs(0, 1 << 0));
+		StringTokenizer st;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
+		System.out.println(dfs(0, 1 << 0));
 	}
 
 	private static int dfs(int cur, int bit) {
 
 		if (Integer.bitCount(bit) == N) {
 
-			if (map.get(cur).get(0).cost == 0) {
-				return INF;
+			if (map[cur][0] == 0) {
+				return map[cur][0] = INF;
 			}
 
-			return map.get(cur).get(0).cost;
+			return map[cur][0];
 		}
 
 		if (dp[cur][bit] != INF) {
 			return dp[cur][bit];
 		}
 
-		for (Node node : map.get(cur)) {
-
-			int next = node.position;
-
-			if ((bit & 1 << next) != 0 || node.cost == 0) {
+		for (int i = 0; i < N; i++) {
+			if ((bit & 1 << i) != 0 || map[cur][i] == 0) {
 				continue;
 			}
 
-			dp[cur][bit] = Math.min(dp[cur][bit], dfs(next, bit | 1 << next) + node.cost);
+			dp[cur][bit] = Math.min(dp[cur][bit], dfs(i, bit | 1 << i) + map[cur][i]);
 		}
 
 		return dp[cur][bit];
-	}
-
-	private static void inputData(BufferedReader br, int N) throws IOException {
-		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				int cost = Integer.parseInt(st.nextToken());
-				map.get(i).add(new Node(j, cost));
-			}
-		}
-	}
-
-	private static void initializeMap(int N) {
-		map = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			map.add(new LinkedList<>());
-		}
-	}
-
-	private static class Node {
-		int position;
-		int cost;
-
-		public Node(int position, int cost) {
-			this.position = position;
-			this.cost = cost;
-		}
 	}
 }
