87c5f0206cee86091e2d070989673fac03d38ce8
kookiedncream <kim76744@naver.com>
2022-06-25 00:05:28 +0900

    refactor: Budget

--- 백준/src/programmers/Budget.java
+++ 백준/src/programmers/Budget.java
@@ -7,43 +7,19 @@ import java.util.Arrays;
  */
 public class Budget {
 	static class Solution {
-		public int solution(int[] d, int M) {
+		public int solution(int[] d, int budget) {
 			int answer = 0;
-			long sum = 0;
 
 			Arrays.sort(d);
 
-			for (int b : d) {
-				sum += b;
-			}
-
-			int n = d.length;
-
-			if (sum < M) {
-				answer = d[n - 1];
-			} else {
-				sum = 0;
-				int left = 0;
-				int right = d[n - 1];
-
-				while (left <= right) {
-					int mid = (left + right) / 2;
-					sum = 0;
-					for (int i = 0; i < n; i++) {
-						if (d[i] < mid)
-							sum += d[i];
-						else
-							sum += mid;
-					}
-					if (sum > M) {
-						right = mid - 1;
-					} else {
-						left = mid + 1;
-						answer = mid;
-					}
+			for (int i = 0; i < d.length; i++) {
+				if (d[i] <= budget) {
+					budget = budget - d[i];
+					answer++;
+					continue;
 				}
+					break;
 			}
-
 			return answer;
 		}
 	}
