18c8301cae1f90ec54073b7c7d818cb7b3934db7
kookiedncream <kim76744@naver.com>
2022-07-08 17:07:16 +0900

    feat: Problem16916
    kmp사용

--- 백준/src/baekjoon/Problem16916.java
+++ 백준/src/baekjoon/Problem16916.java
@@ -3,23 +3,59 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Pattern;
 
 /**
  * 출처: https://www.acmicpc.net/problem/16916
  */
 public class Problem16916 {
+
+	private static int answer;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String S = br.readLine();
-		String P = br.readLine();
+		char[] parent = br.readLine().toCharArray();
+		char[] pattern = br.readLine().toCharArray();
 
-		String regex = ".*" + P + ".*";
-		boolean matches = S.matches(regex);
-		if (matches) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
+		kmp(parent, pattern);
+
+		System.out.println(answer);
+	}
+
+	private static void kmp(char[] parent, char[] pattern) {
+		int[] table = makeTable(pattern);
+		int parentSize = parent.length;
+		int patternSize = pattern.length;
+
+		int j = 0;
+		for (int i = 0; i < parentSize; i++) {
+			while (j > 0 && parent[i] != pattern[j]) {
+				j = table[j - 1];
+			}
+			if (parent[i] == pattern[j]) {
+				if (j == patternSize - 1) {
+					answer = 1;
+					j = table[j];
+				} else {
+					j++;
+				}
+			}
 		}
+	}
+
+	private static int[] makeTable(char[] pattern) {
+		int patternSize = pattern.length;
+		int[] arr = new int[patternSize];
+		int j = 0;
+
+		for (int i = 1; i < patternSize; i++) {
+			while (j > 0 && pattern[i] != pattern[j]) {
+				j = arr[j - 1];
+			}
+
+			if (pattern[i] == pattern[j]) {
+				arr[i] = ++j;
+			}
+		}
+		return arr;
 	}
 }
