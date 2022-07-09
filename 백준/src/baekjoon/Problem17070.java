f625d31340ccbf48bd8c6f983ea877b3298f2ac4
kookiedncream <kim76744@naver.com>
2022-07-09 15:32:05 +0900

    refactor: Problem17070
    dp로 변경

--- 백준/src/baekjoon/Problem17070.java
+++ 백준/src/baekjoon/Problem17070.java
@@ -3,30 +3,16 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
  * 출처: https://www.acmicpc.net/problem/17070
  */
 public class Problem17070 {
-	private static final int[][] dr = {
-			{0, 1}, // 가로 0
-			{1, 1}, // 세로 1
-			{0, 1, 1} // 대각선 2
-	};
-
-	private static final int[][] dc = {
-			{1, 1}, // 가로 0 -> 가로 0, 대각선 1
-			{0, 1}, // 세로 1 -> 세로 0, 대각선 1
-			{1, 0, 1} // 대각선 2 -> 가로 0, 세로 1, 대각선 2
-	};
 
 	private static int N;
 	private static int[][] map;
-	private static boolean[][][] isVisited;
-	private static int answer;
+	private static int[][][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,79 +20,40 @@ public class Problem17070 {
 
 		initializeMap(br);
 
-		isVisited = new boolean[N][N][3];
-		bfs(0, 1, 0);
+		dp = new int[N + 1][N + 1][3];
+		dp[1][2][0] = 1;
+		dynamic();
 
-		System.out.println(answer);
+		System.out.println(dp[N][N][0] + dp[N][N][1] + dp[N][N][2]);
 	}
 
-	private static void bfs(int r, int c, int type) {
-		if (r == N - 1 && c == N - 1) {
-			answer++;
-			return;
-		}
-
-		for (int i = 0; i < dr[type].length; i++) {
-			int nr = r + dr[type][i];
-			int nc = c + dc[type][i];
-
-			if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-				continue;
-			}
-
-			if (map[nr][nc] == 1) {
-				continue;
-			}
-
-			if ((nr - r == nc - c) && (map[nr - 1][nc] == 1 || map[nr][nc - 1] == 1)) {
-				continue;
-			}
-
-			if (type != 2) {
-				if (i == 0) {
-					bfs(nr, nc, type);
-					continue;
-				} else {
-					bfs(nr, nc, 2);
+	private static void dynamic() {
+		for (int r = 1; r <= N; r++) {
+			for (int c = 1; c <= N; c++) {
+				if (map[r][c] == 1) {
 					continue;
 				}
-			}
+				dp[r][c][0] += dp[r][c - 1][0] + dp[r][c - 1][2];
+				dp[r][c][1] += dp[r - 1][c][1] + dp[r - 1][c][2];
 
-			if (i == 0) {
-				bfs(nr, nc, 0);
-				continue;
-			}
+				if (map[r - 1][c] == 1 || map[r][c - 1] == 1) {
+					continue;
+				}
 
-			if (i == 1) {
-				bfs(nr, nc, 1);
-				continue;
+				dp[r][c][2] += dp[r - 1][c - 1][0] + dp[r - 1][c - 1][1] + dp[r - 1][c - 1][2];
 			}
-
-			bfs(nr, nc, 2);
 		}
 	}
 
 	private static void initializeMap(BufferedReader br) throws IOException {
-		map = new int[N][N];
+		map = new int[N + 1][N + 1];
 
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
+			for (int j = 1; j <= N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
-		}
-	}
-
-	private static class Node {
-		int row;
-		int col;
-		int type;
-
-		public Node(int row, int col, int type) {
-			this.row = row;
-			this.col = col;
-			this.type = type;
 		}
 	}
 }
