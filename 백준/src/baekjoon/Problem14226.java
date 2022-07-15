53cead865f4d4ed841ec42d116d8f1d914ae0e79
kookiedncream <kim76744@naver.com>
2022-07-16 00:18:11 +0900

    refactor: Problem14226

--- 백준/src/baekjoon/Problem14226.java
+++ 백준/src/baekjoon/Problem14226.java
@@ -3,57 +3,83 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
+import static java.util.Comparator.*;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/14226
+ */
 public class Problem14226 {
-	static boolean visited[][] = new boolean[1001][1001];
+
+	private static int[] arr;
+	private static boolean[][] isVisited;
+	private static int answer = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int s = Integer.parseInt(br.readLine());
+		int S = Integer.parseInt(br.readLine());
 
-		bfs(s);
+		isVisited = new boolean[1001][1001];
+		bfs(S);
 
+		System.out.println(answer);
 	}
 
-	static void bfs(int end) {
-		Queue<emoticon> q = new LinkedList<>();
-		q.offer(new emoticon(0, 1, 0));
-		visited[0][1] = true;
+	private static void bfs(int s) {
+		PriorityQueue<Node> pq = new PriorityQueue<>(comparingInt(n -> n.time));
 
-		while (!q.isEmpty()) {
-			emoticon cur = q.poll();
+		isVisited[0][0] = true;
+		isVisited[1][0] = true;
+		pq.add(new Node(1, 0, 0));
 
-			if (cur.screen == end) {
-				System.out.println(cur.second);
-				return;
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+			int curV = cur.value;
+			int curT = cur.time;
+			int curBoard = cur.clipboard;
+
+			if (curV == s) {
+				answer = curT;
+				break;
 			}
 
-			q.offer(new emoticon(cur.screen, cur.screen, cur.second + 1));
-
-			if (cur.clipboard != 0 && cur.screen + cur.clipboard <= end && !visited[cur.clipboard][cur.screen + cur.clipboard]) {
-				q.offer(new emoticon(cur.clipboard, cur.screen + cur.clipboard, cur.second + 1));
-				visited[cur.clipboard][cur.screen + cur.clipboard] = true;
+			// 하나빼기
+			if (curV > 1 && !isVisited[curV - 1][curBoard]) {
+				isVisited[curV - 1][curBoard] = true;
+				pq.add(new Node(curV - 1, curBoard, curT + 1));
 			}
 
-			if (1 <= cur.screen && !visited[cur.clipboard][cur.screen - 1]) {
-				q.offer(new emoticon(cur.clipboard, cur.screen - 1, cur.second + 1));
-				visited[cur.clipboard][cur.screen - 1] = true;
+			if (curV + curBoard > 1000) {
+				continue;
+			}
+
+			// 클립보드에 저장
+			if (!isVisited[curV][curV]) {
+				isVisited[curV][curV] = true;
+				pq.add(new Node(curV, curV, curT + 1));
+			}
+
+			// 붙여넣기
+			if (curBoard != 0 && !isVisited[curV + curBoard][curBoard]) {
+				isVisited[curV + curBoard][curBoard] = true;
+				pq.add(new Node(curV + curBoard, curBoard, curT + 1));
 			}
 		}
-
 	}
-}
 
-class emoticon {
-	int clipboard = 0;
-	int screen = 0;
-	int second = 0;
 
-	emoticon(int clipboard, int screen, int second) {
-		this.clipboard = clipboard;
-		this.screen = screen;
-		this.second = second;
+	private static class Node {
+		int value;
+		int clipboard;
+		int time;
+
+		public Node(int value, int clipboard, int time) {
+			this.value = value;
+			this.clipboard = clipboard;
+			this.time = time;
+		}
 	}
 }
