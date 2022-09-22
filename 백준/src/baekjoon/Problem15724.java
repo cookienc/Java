fe49805ad092f5b4b928e51fc7352578b5a8ef70
kookiedncream <kim76744@naver.com>
2022-09-22 16:54:21 +0900

    feat: Problem15724

---
+++ 백준/src/baekjoon/Problem15724.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/15724
+ */
+public class Problem15724 {
+	private static int[][] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N + 1][M + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= M; j++) {
+				arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + Integer.parseInt(st.nextToken());
+			}
+		}
+
+		int K = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		while (K-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+
+			sb.append(arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
