cd337e0931a1cf1d187c5b24a233fa3b8ab764ce
cookienc <kim76744@naver.com>
2022-10-31 18:24:57 +0900

    feat: Problem2914

---
+++ 백준/src/baekjoon/Problem2914.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 출처: https://www.acmicpc.net/problem/2914
+ */
+public class Problem2914 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int A = Integer.parseInt(st.nextToken());
+		int i = Integer.parseInt(st.nextToken());
+
+		System.out.println(A * (i - 1) + 1);
+	}
+}
