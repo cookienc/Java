98f618d6c3e0f79a1ef260255f41908d42e49709
Ethan <kim76744dev@gmail.com>
2023-06-14 14:09:30 +0900

    refactor: 주식가격
    스택으로 업데이트

--- 백준/src/programmers/StockPrice.java
+++ 백준/src/programmers/StockPrice.java
@@ -1,22 +1,38 @@
 package programmers;
 
+import java.util.Stack;
+
 /**
  * 출처: https://programmers.co.kr/learn/courses/30/lessons/42584
  */
 public class StockPrice {
-	static class Solution {
-		public int[] solution(int[] prices) {
-			int[] ans = new int[prices.length];
+    static class Solution {
 
-			for (int i = 0; i < prices.length; i++) {
-				for (int j = i + 1; j < prices.length; j++) {
-					ans[i]++;
-					if (prices[i] > prices[j])
-						break;
+        private static int[] answers;
 
-				}
-			}
-			return ans;
-		}
-	}
+        public int[] solution(int[] prices) {
+            int length = prices.length;
+            answers = new int[length];
+
+            Stack<Integer> stack = new Stack<>();
+            int idx = 0;
+
+            stack.push(idx);
+            for (idx = 1; idx < length; idx++) {
+                while (!stack.isEmpty() && prices[idx] < prices[stack.peek()]) {
+                    int targetIdx = stack.pop();
+                    answers[targetIdx] = idx - targetIdx;
+                }
+
+                stack.push(idx);
+            }
+
+            while (!stack.isEmpty()) {
+                int target = stack.pop();
+                answers[target] = length - target - 1;
+            }
+
+            return answers;
+        }
+    }
 }
