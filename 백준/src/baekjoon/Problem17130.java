66cccbe734579c4bf5a4b04247e31ce44778ad6b
Ethan <kim76744dev@gmail.com>
2023-07-04 08:31:19 +0900

    refactor: Problem17130
    dp로 변경

--- 백준/src/baekjoon/Problem17130.java
+++ 백준/src/baekjoon/Problem17130.java
@@ -3,125 +3,90 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Problem17130 {
 
-    private static final int CAN_MOVE = 0;
-    private static final int CARROT = 1;
-    private static final int WALL = 2;
-    private static final int HOLE = 9;
-    private static final int RABBIT = 3;
-    private static int[][] map;
-    private static int[][] carrotMap;
-    private static int[] dr = {0, -1, 1};
-    private static int[] dc = {1, 1, 1};
+    private static final char CAN_MOVE = '.';
+    private static final char CARROT = 'C';
+    private static final char WALL = '#';
+    private static final char HOLE = 'O';
+    private static final char RABBIT = 'R';
+    private static char[][] map;
     private static int R;
     private static int C;
     private static int answer = -1;
+    private static int[] dr = {-1, 0, 1};
+    private static int[] dc = {-1, -1, -1};
+    private static int[][] dp;
 
     public static void main(String[] args) throws IOException {
-        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        map = new int[R][C];
-        carrotMap = new int[R][C];
-        int startR = 0;
-        int startC = 0;
+        map = new char[R][C];
+        Queue<int[]> q = new LinkedList<>();
+        int rr = 0;
+        int rc = 0;
         for (int r = 0; r < R; r++) {
-            final String value = br.readLine();
+            final String value = bufferedReader.readLine();
             for (int c = 0; c < C; c++) {
                 final char ch = value.charAt(c);
-                if (ch == '.') {
-                    map[r][c] = CAN_MOVE;
-                    continue;
+                if (ch == HOLE || ch == WALL) {
+                    q.add(new int[]{r, c});
                 }
 
-                if (ch == 'C') {
-                    map[r][c] = CARROT;
-                    continue;
+                if (ch == RABBIT) {
+                    rr = r;
+                    rc = c;
                 }
-
-                if (ch == '#') {
-                    map[r][c] = WALL;
-                    continue;
-                }
-
-                if (ch == 'O') {
-                    map[r][c] = HOLE;
-                    continue;
-                }
-
-                startR = r;
-                startC = c;
-                map[r][c] = RABBIT;
+                map[r][c] = ch;
             }
         }
 
-        findRoute(startR, startC);
+        dp = new int[R][C];
+        for (int r = 0; r < R; r++) {
+            Arrays.fill(dp[r], -1);
+        }
+
+        dp[rr][rc] = 0;
+
+        for (int c = rc + 1; c < C; c++) {
+            for (int r = 0; r < R; r++) {
+                if (map[r][c] == WALL) {
+                    continue;
+                }
+
+                for (int i = 0; i < 3; i++) {
+                    int br = r + dr[i];
+                    int bc = c + dc[i];
+
+                    if (isOver(br, bc)) {
+                        continue;
+                    }
+
+                    if (map[br][bc] == CARROT && dp[br][bc] != -1) {
+                        dp[r][c] = Math.max(dp[r][c], dp[br][bc] + 1);
+                        continue;
+                    }
+
+                    dp[r][c] = Math.max(dp[r][c], dp[br][bc]);
+                }
+            }
+        }
+
+        for (int[] hole : q) {
+            answer = Math.max(answer, dp[hole[0]][hole[1]]);
+        }
         System.out.println(answer);
     }
 
-    private static void findRoute(final int startR, final int startC) {
-
-        Queue<Rabbit> rabbits = new LinkedList<>();
-        rabbits.add(new Rabbit(startR, startC, 0));
-        boolean[][] isVisited = new boolean[R][C];
-        isVisited[startR][startC] = true;
-
-        while (!rabbits.isEmpty()) {
-            final Rabbit rabbit = rabbits.poll();
-
-            if (carrotMap[rabbit.r][rabbit.c] > rabbit.carrot) {
-                continue;
-            }
-
-            for (int i = 0; i < 3; i++) {
-                final int nr = rabbit.r + dr[i];
-                final int nc = rabbit.c + dc[i];
-
-                if (nr < 0 || nc < 0 || nr >= R || nc >= C) {
-                    continue;
-                }
-
-                if (map[nr][nc] == WALL) {
-                    continue;
-                }
-
-                int carrot = rabbit.carrot;
-                if (map[nr][nc] == CARROT) {
-                    carrot++;
-                }
-
-                if (isVisited[nr][nc] && carrotMap[nr][nc] >= carrot) {
-                    continue;
-                }
-
-                isVisited[nr][nc] = true;
-                carrotMap[nr][nc] = carrot;
-
-                if (map[nr][nc] == HOLE) {
-                    answer = Math.max(answer, carrot);
-                }
-
-                rabbits.add(new Rabbit(nr, nc, carrot));
-            }
-        }
-    }
-
-    private static class Rabbit {
-        int r;
-        int c;
-        int carrot;
-
-        public Rabbit(final int r, final int c, final int carrot) {
-            this.r = r;
-            this.c = c;
-            this.carrot = carrot;
-        }
+    private static boolean isOver(final int br, final int bc) {
+        return br < 0 || bc < 0 || br >= R || bc >= C;
     }
 }
