8550738071569ebd471591bc288710a67f4f4495
Ethan <kim76744dev@gmail.com>
2023-07-08 09:46:56 +0900

    refactor: Problem21921

--- 백준/src/baekjoon/Problem21921.java
+++ 백준/src/baekjoon/Problem21921.java
@@ -9,54 +9,45 @@ import java.util.StringTokenizer;
  * 출처: https://www.acmicpc.net/problem/21921
  */
 public class Problem21921 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken()) - 1;
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int cnt = 0;
-		int max = 0;
+        int[] sum = new int[N];
+        sum[0] = arr[0];
+        for (int i = 1; i < N; i++) {
+            sum[i] += sum[i - 1] + arr[i];
+        }
 
-		int start = 0;
-		int end = 0;
-		int sum = arr[0];
+        int max = sum[X - 1];
+        int maxCnt = 1;
+        for (int i = X; i < N; i++) {
+            final int curSum = sum[i] - sum[i - X];
+            if (curSum > max) {
+                max = curSum;
+                maxCnt = 1;
+                continue;
+            }
 
-		while (start <= end) {
-			while (end - start < X) {
-				sum += arr[++end];
-			}
+            if (curSum == max) {
+                maxCnt++;
+            }
+        }
 
-			if (sum == max) {
-				cnt++;
-			}
-
-			if (sum > max) {
-				max = sum;
-				cnt = 1;
-			}
-
-			sum -= arr[start++];
-			if (end < N - 1) {
-				sum += arr[++end];
-			} else {
-				break;
-			}
-		}
-
-
-		if (max == 0) {
+		if (max != 0) {
+			System.out.println(max);
+			System.out.println(maxCnt);
+		} else {
 			System.out.println("SAD");
-			return;
 		}
-		System.out.println(max);
-		System.out.println(cnt);
-	}
+    }
 }
