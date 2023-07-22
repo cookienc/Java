1394744a39835b03ff968cd6fdcac4006f647771
Ethan <kim76744dev@gmail.com>
2023-07-22 19:55:07 +0900

    refactor: Problem2579

--- 백준/src/baekjoon/Problem2579.java
+++ 백준/src/baekjoon/Problem2579.java
@@ -3,50 +3,40 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Problem2579 {
 
-    private static int[] array;
-    private static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) throws Exception {
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
-
-        int num = Integer.parseInt(bufferedReader.readLine());
-
-        if (num < 1 || num > 300) {
-            throw new Exception("오류");
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        if (N == 0) {
+            System.out.println(0);
+            return;
         }
 
-        array = new int[num + 1];
-        dp = new Integer[num + 1];
-
-        inputData(bufferedReader, num);
-
-        dp[0] = 0;
-        dp[1] = array[1];
-
-        if (num > 1) {
-            dp[2] = array[1] + array[2];
+        if (N == 1) {
+            System.out.println(arr[N]);
+            return;
         }
 
-        System.out.println(findDp(num));
-    }
-
-    private static void inputData(BufferedReader bufferedReader, int num) throws IOException {
-        for (int i = 1; i <= num; i++) {
-            array[i] = Integer.parseInt(bufferedReader.readLine());
+        if (N == 2) {
+            System.out.println(arr[1] + arr[2]);
+            return;
         }
-    }
 
-    private static int findDp(int num) {
+        int[] dp = new int[N + 1];
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
 
-        if (dp[num] == null) {
-            dp[num] = Math.max(findDp(num - 2), findDp(num - 3) + array[num - 1]) + array[num];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
         }
-        return dp[num];
+
+        System.out.println(dp[N]);
     }
 }
