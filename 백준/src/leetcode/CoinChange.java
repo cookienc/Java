43100025cdee50c0c0b1fe831120e059d79a4ea1
Ethan <kim76744dev@gmail.com>
2023-07-27 09:38:23 +0900

    feat: CoinChange

---
+++ 백준/src/leetcode/CoinChange.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class CoinChange {
+
+    public static void main(String[] args) {
+        final int[] coins = {2, 3, 5};
+        System.out.println(coinChange(coins, 11));
+    }
+
+    public static int coinChange(int[] coins, int amount) {
+
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                int value = i - coin;
+                if (value < 0) {
+                    continue;
+                }
+
+                if (value == 0) {
+                    dp[i] = 1;
+                    continue;
+                }
+
+                if (dp[i] == 0) {
+                    dp[i] = dp[value] + 1;
+                } else {
+                    if (dp[value] == -1) {
+                        continue;
+                    }
+                    dp[i] = Math.min(dp[i], dp[value] + 1);
+                }
+            }
+
+            if (dp[i] == 0) {
+                dp[i] = -1;
+            }
+        }
+
+        return dp[amount];
+    }
+}
