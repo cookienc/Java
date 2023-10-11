725b82bd31479d1945fc3887aff28d27739f6e20
Ethan <kim76744dev@gmail.com>
2023-10-11 09:01:03 +0900

    feat: Problem15989

--- 백준/src/baekjoon/Problem15989.java
+++ 백준/src/baekjoon/Problem15989.java
@@ -9,33 +9,32 @@ import java.io.InputStreamReader;
  */
 public class Problem15989 {
 
-	private static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		makeArr(10000);
-		int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+        dp[1][1] = 1;
+        dp[1][2] = 0;
+        dp[1][3] = 0;
 
-		while (T-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
-		}
-	}
+        dp[2][1] = 1;
+        dp[2][2] = 1;
+        dp[2][3] = 0;
 
-	private static void makeArr(int n) {
-		dp = new int[10001][4];
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
 
-		dp[1][1] = 1;
-		dp[2][1] = 1;
-		dp[2][2] = 1;
-		dp[3][1] = 1;
-		dp[3][2] = 1;
-		dp[3][3] = 1;
+        for (int i = 4; i <= 10000; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];;
+        }
 
-		for (int i = 4; i <= n; i++) {
-			dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
-			dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
-			dp[i][1] = dp[i - 1][1];
-		}
-	}
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            final int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n][1] + dp[n][2] + dp[n][3]);
+        }
+    }
 }
