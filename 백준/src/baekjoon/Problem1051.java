1c7b169bae7d25c10112bc041504d69621a0bd46
Ethan <kim76744dev@gmail.com>
2023-10-16 22:46:49 +0900

    feat: Problem1051

--- 백준/src/baekjoon/Problem1051.java
+++ 백준/src/baekjoon/Problem1051.java
@@ -7,44 +7,48 @@ import java.util.StringTokenizer;
 
 public class Problem1051 {
 
-    private static int[][] map;
-
     public static void main(String[] args) throws IOException {
         final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        final StringTokenizer st = new StringTokenizer(br.readLine());
         final int N = Integer.parseInt(st.nextToken());
         final int M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        for (int r = 0; r < N; r++) {
-            final String s = br.readLine();
-            for (int c = 0; c < M; c++) {
-                map[r][c] = s.charAt(c) - '0';
+        int[][] map = new int[M][N];
+        for (int i = 0; i < N; i++) {
+            final char[] chars = br.readLine().toCharArray();
+            for (int j = 0; j < M; j++) {
+                map[j][i] = chars[j] - '0';
             }
         }
 
-        int max = 0;
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < M; c++) {
-                final int criteria = map[r][c];
-                for (int nc = c; nc < M; nc++) {
-                    if (map[r][nc] != criteria) {
+        final int maxSize = Math.min(M, N);
+
+        int answer = 0;
+        for (int s = 0; s <= maxSize; s++) {
+            for (int c = 0; c < N; c++) {
+                final int h = c + s;
+                if (h >= N) {
+                    continue;
+                }
+
+                for (int r = 0; r < M; r++) {
+                    final int w = r + s;
+
+                    if (w >= M) {
                         continue;
                     }
 
-                    final int diff = nc - c;
-                    if (r + diff >= N) {
-                        continue;
-                    }
+                    final int a = map[r][c];
+                    final int b = map[w][c];
+                    final int cc = map[r][h];
+                    final int d = map[w][h];
 
-                    if (map[r + diff][c] == criteria && map[r + diff][nc] == criteria) {
-                        max = Math.max(max, (diff + 1) * (diff + 1));
+                    if (a == b && d == cc && b == cc) {
+                        answer = Math.max(answer, s + 1);
                     }
                 }
             }
         }
-
-        System.out.println(max);
+        System.out.println((int) Math.pow(answer, 2));
     }
 }
