78599849b307a2cae1dc1dad194a0521a1524543
Ethan <kim76744dev@gmail.com>
2024-02-04 15:56:42 +0900

    refactor: ArcheologysGreatestDiscoveries
    다시풀기

--- 백준/src/programmers/ArcheologysGreatestDiscoveries.py
+++ 백준/src/programmers/ArcheologysGreatestDiscoveries.py
@@ -1,38 +1,37 @@
-def turn(clockHands, r, c, cnt):
-    dr, dc = [0, 1, 0], [1, 0, -1]
-    clockHands[r][c] = (clockHands[r][c] + cnt) % 4
-
-    for d in range(3):
-        nr, nc = r + dr[d], c + dc[d]
-        if nr < 0 or nr >= len(clockHands) or nc < 0 or nc >= len(clockHands):
+def rotate(clockHands, r, c, roationCount):
+    moves = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    clockHands[r][c] = (clockHands[r][c] + roationCount) % 4
+    for i in range(4):
+        nr = r + moves[i][0]
+        nc = c + moves[i][1]
+        if nr < 0 or nc < 0 or nr >= len(clockHands) or nc >= len(clockHands):
             continue
-        clockHands[nr][nc] = (clockHands[nr][nc] + cnt) % 4
+        clockHands[nr][nc] = (clockHands[nr][nc] + roationCount) % 4
 
 
 def solution(clockHands):
     n = len(clockHands)
+    answer = 3 ** n + 1
     origin = clockHands
-    answer = 3 ** (n * n)
-    for case in range(4 ** n):
+    for rotations in range(4 ** n):
         clockHands = [row[:] for row in origin]
-        total_cnt = 0
+        totalRotations = 0
         for c in range(n):
-            cnt = case % 4
-            total_cnt += cnt
-            turn(clockHands, 0, c, cnt)
-            case >>= 2
+            rotationCount = rotations % 4
+            totalRotations += rotationCount
+            rotate(clockHands, 0, c, rotationCount)
+            rotations >>= 2
 
         for r in range(1, n):
             for c in range(n):
-                prev_row = clockHands[r - 1][c]
-                cnt = (4 - prev_row) % 4
-                total_cnt += cnt
-                if cnt == 0:
+                rotationCount = abs(clockHands[r - 1][c] - 4) % 4
+                if rotationCount == 0:
                     continue
-                turn(clockHands, r, c, cnt)
+                totalRotations += rotationCount
+                rotate(clockHands, r, c, rotationCount)
 
         if sum(clockHands[-1]) == 0:
-            answer = min(answer, total_cnt)
+            answer = min(answer, totalRotations)
 
     return answer
 
